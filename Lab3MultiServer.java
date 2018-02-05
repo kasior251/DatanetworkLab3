44a9ea02c73651342886bfa5af7c526ec3904a41
kasior251 <s305327@stud.hioa.no>
2018-02-05 16:58:09 +0100

    converter + server

--- Lab3MultiServer.java
+++ Lab3MultiServer.java
@@ -1,11 +1,9 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.Buffer;
+import java.util.Scanner;
 
 public class Lab3MultiServer {
 
@@ -23,9 +21,11 @@ public class Lab3MultiServer {
             }
         }
 
+
         System.out.println("Hi I am the Multi Client TCP Server");
         try (
                 ServerSocket serverSocket = new ServerSocket(portNumber);
+
                 )
         {
             String receivedText;
@@ -36,6 +36,7 @@ public class Lab3MultiServer {
                 ClientServer clientServer = new Lab3MultiServer.ClientServer(serverSocket.accept());
                 clientServer.start();
             }
+
 
         }
         catch (IOException e) {
@@ -50,6 +51,12 @@ public class Lab3MultiServer {
         InetAddress clientAddres;
         int serverPort, clientPort;
 
+        String file = "C:\\Users\\Kasia\\IdeaProjects\\DatanetworkLab3Part1\\src\\currencies.csv";
+        BufferedReader br = null;
+        String line = "";
+        String splitBy = ",";
+        String[] currency = new String[10];
+
         public ClientServer(Socket connectSocket) {
             this.connectSocket = connectSocket;
             clientAddres = connectSocket.getInetAddress();
@@ -58,6 +65,27 @@ public class Lab3MultiServer {
         }
 
         public void run() {
+
+            try {
+                br = new BufferedReader(new FileReader(file));
+                while ((line = br.readLine()) != null) {
+                    currency = line.split(splitBy);
+                }
+            }
+            catch (FileNotFoundException e) {
+                System.err.println("File not found");
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
             try (
                     PrintWriter out = new PrintWriter(connectSocket.getOutputStream(), true);
                     BufferedReader in = new BufferedReader(new InputStreamReader(connectSocket.getInputStream()));
@@ -65,8 +93,9 @@ public class Lab3MultiServer {
             {
                 String receivedText;
                 while (((receivedText = in.readLine()) != null)) {
+                    System.out.println("Type in first currency, amount and currency to convert to (without white spaces)");
                     System.out.println("Client [" + clientAddres.getHostAddress() +  ":" + clientPort +"] > " + receivedText);
-                    String outText = receivedText.toUpperCase();
+                    String outText = Converter.convert(receivedText.toUpperCase(), currency);
                     out.println(outText);
                     System.out.println("I (Server) [" + connectSocket.getLocalAddress().getHostAddress() + ":" + serverPort +"] > " + outText);
                 }
